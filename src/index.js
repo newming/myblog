a4e1ca40051e27eaab7ef7c6ebe222c9480cae2e
newming <a977527479@sina.com>
2017-03-05 14:34:13 +0800

    deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './routes';
-import 'highlight.js/styles/atom-one-light.css';
-import './main.css';
-import './post.css';
-render(<Routers />,document.getElementById('root'));
