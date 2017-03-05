3d9fa3ca715f9a3c4ea3e3028cb77689c934033c
newming <a977527479@sina.com>
2017-03-05 14:10:02 +0800

    fini

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route, browserHistory, IndexRoute } from 'react-router';
+import {Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import App from './App';
 import Home from './components/Home';
@@ -10,7 +10,7 @@ import Post from './components/Post';
 
 export default function () {
   return (
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Home} />
         <Route path='blog' component={Blog} />
