4f7a43560fe63cfe5c0a1f3b84cdfeb61c9612bf
newming <a977527479@sina.com>
2017-03-05 15:18:09 +0800

    modify

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ class Home extends React.Component{
     return(
       <div className='home'>
         <div className='home-cover'>
-          <h1>I'M NEWMING</h1>
-          <p>FrontEnd Web Developer</p>
-          <a href='https://github.com/newming'>MY GITHUN</a>
+          <div>
+            <h1>I'M NEWMING</h1>
+            <p>FrontEnd Web Developer</p>
+            <a href='https://github.com/newming'>MY GITHUN</a>
+          </div>
         </div>
       </div>
     )
